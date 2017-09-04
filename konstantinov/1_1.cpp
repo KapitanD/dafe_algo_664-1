eb8abb905cfe79ab73e817f6710e616b7dd81544
CI31X <31621941+CI31X@users.noreply.github.com>
2017-09-04 16:23:57 +0300

    Update 1_1.cpp

--- konstantinov/1_1.cpp
+++ konstantinov/1_1.cpp
@@ -1,1 +1,22 @@
-fafadffdsfsf
+#include <iostream>
+using namespace std;
+
+int fact (int n, int k)
+{ 
+ int p=0;
+ while (n)
+ {
+  n=n/k;
+  p+=n;
+  
+ }
+  return p;
+}
+int main()
+{
+ int n;
+ cin>>n;
+
+ cout<<fact(n,5);
+ return 0;
+}
